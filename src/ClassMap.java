f281e123209f419ddd6a4d803ea9bd5048ac6b21
aki-27to <120763272+aki-27to@users.noreply.github.com>
2022-12-24 13:07:04 +0900

    ご指摘いただいた２点について修正

--- src/ClassMap.java
+++ src/ClassMap.java
@@ -9,8 +9,8 @@ public class ClassMap {
         classMember.put("田中", 85);
         classMember.put("鈴木", 82);
         classMember.put("佐藤", 94);
-        classMember.put("太田" , 78);
-        classMember.put("石田" , 56);
+        classMember.put("太田", 78);
+        classMember.put("石田", 56);
         //keySetMethodを使用してmapの全てのキーを出力
         for (String key : classMember.keySet()) {
             System.out.println(key + "はAクラスです");
@@ -23,18 +23,18 @@ public class ClassMap {
 
         try {
             //getMethodを使用、キーに対応する値を取得する。
-            //下記に例外キーを定義。下記以外が選択された場合、catchする。
-            Integer tanaka = classMember.get("田中"); //getで対応する値の取得
-            Integer suzuki = classMember.get("鈴木");
-            Integer satou = classMember.get("佐藤");
-            Integer oota = classMember.get("太田");
-            Integer ishida = classMember.get("石田");
-            Integer takeda = classMember.get("武田");
-            //武田の値をgetしようとすると、"武田はメンバー外です"を出力する
-            System.out.println("合計得点は" +( tanaka + suzuki + satou + oota + takeda  )+ "点です。");
+            //下記にキーを定義。下記で定義したキーがMapに存在しない場合はcatchする。
+            Integer tanakaScore = classMember.get("田中");
+            Integer suzukiScore = classMember.get("鈴木");
+            Integer satouScore = classMember.get("佐藤");
+            Integer ootaScore = classMember.get("太田");
+            Integer ishidaScore = classMember.get("石田");
+            Integer takedaScore = classMember.get("武田");
+            //武田の値をgetしようとすると、"武田はメンバー外です"を出力する。武田を消すと総得点を計算する。
+            System.out.println("合計得点は" + (tanakaScore + suzukiScore + satouScore + ootaScore + ishidaScore + takedaScore) + "点です。");
         } catch (NullPointerException e) {
             System.out.println("\n");
-            System.out.println( "武田はメンバー外です");
+            System.out.println("武田はメンバー外です");
         }
     }
 }
